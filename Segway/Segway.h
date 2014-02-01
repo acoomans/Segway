4e05b16220b48fd001d91b69dc1eafa89d4782c8
Arnaud Coomans <arnaud.coomans@gmail.com>
2014-01-31 16:17:08 -0800

    Initial commit

---
+++ Segway/Segway.h
@@ -0,0 +1,13 @@
+//
+//  Segway.h
+//  Segway
+//
+//  Created by Arnaud Coomans on 31/01/14.
+//  Copyright (c) 2014 Arnaud Coomans. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Segway : NSObject
+
+@end
---
+++ Segway/Segway.m
@@ -0,0 +1,13 @@
+//
+//  Segway.m
+//  Segway
+//
+//  Created by Arnaud Coomans on 31/01/14.
+//  Copyright (c) 2014 Arnaud Coomans. All rights reserved.
+//
+
+#import "Segway.h"
+
+@implementation Segway
+
+@end
