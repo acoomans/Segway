99b56923d28b838d0c3d290beb28b888a7f6aea6
Arnaud Coomans <arnaud.coomans@gmail.com>
2014-02-02 17:50:20 -0800

    Fixed popover segue tests

--- SegwayTests/SegueTemplates/ACStoryboardPopoverSegueTests.m
+++ SegwayTests/SegueTemplates/ACStoryboardPopoverSegueTests.m
@@ -49,53 +49,33 @@
 
 - (void)testRegisterAndPerformFromBarButtonItem {
     
-    dispatch_semaphore_t mutex = dispatch_semaphore_create(0);
-    
     UIWindow *window = [[UIWindow alloc] initWithFrame:CGRectMake(0, 0, 1024, 1024)];
     
     UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Navigation" bundle:[NSBundle bundleForClass:self.class]];
     UINavigationController *navigationController = [storyboard instantiateInitialViewController];
-    UIViewController *viewControllerA = navigationController.topViewController;
-    NSAssert(viewControllerA != nil, @"No top view controller present");
-    
-    viewControllerA.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone
-                                                                                                      target:nil
-                                                                                                      action:nil];
     
     window.rootViewController = navigationController;
-    [window addSubview:viewControllerA.view];
-    NSAssert(viewControllerA.view.window != nil, @"view has no window");
     [window makeKeyAndVisible];
     
+    UIViewController *viewControllerA = navigationController.topViewController;
+    NSAssert(viewControllerA != nil, @"No top view controller present");
+    NSAssert(viewControllerA.navigationController.navigationBar != nil, @"view has no navigation bar");
     
-    //NSAssert(viewControllerA.navigationController.navigationBar != nil, @"view has no navigation bar");
-    UIBarButtonItem *barButtonItem = viewControllerA.navigationItem.rightBarButtonItem;
-    NSAssert(barButtonItem != nil, @"viewControllerA has no right bar button item");
+    UIBarButtonItem *barButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone
+                                                                                   target:nil
+                                                                                   action:nil];
+    viewControllerA.navigationItem.rightBarButtonItem = barButtonItem;
+    NSAssert(viewControllerA.navigationItem.rightBarButtonItem != nil, @"viewControllerA has no right bar button item");
     
     [viewControllerA addSegueTemplate:[[ACStoryboardPopoverSegueTemplate alloc] initWithIdentifier:@"segueIdentifier"
                                                                destinationViewControllerIdentifier:@"viewControllerB"
                                                                                anchorBarButtonItem:barButtonItem
                                                                           permittedArrowDirections:UIPopoverArrowDirectionAny]];
     
-    /*
-     [viewControllerA addSegueTemplate:[[ACStoryboardPopoverSegueTemplate alloc] initWithIdentifier:@"segueIdentifier"
-     destinationViewControllerIdentifier:@"viewControllerB"
-     anchorBarButtonItem:barButtonItem
-     permittedArrowDirections:UIPopoverArrowDirectionAny]];
-     */
-    
     [viewControllerA performSegueWithIdentifier:@"segueIdentifier" sender:self];
     
-    
-    
-    dispatch_semaphore_wait(mutex, dispatch_time(DISPATCH_TIME_NOW, 3*NSEC_PER_SEC));
-    
-    XCTAssertNotNil(viewControllerA.presentedViewController);
-    XCTAssert([viewControllerA.presentedViewController.title isEqualToString:@"B"]);
-    
-    
-    //viewControllerA.view = nil;
-    //window.rootViewController= nil;
+    XCTAssertNotNil(viewControllerA.popoverViewController);
+    XCTAssert([viewControllerA.popoverViewController.contentViewController.title isEqualToString:@"B"]);
 }
 
 @end
