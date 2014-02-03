bab2e566b6e7c7f1cedce3e637751d42a44d60ad
Arnaud Coomans <arnaud.coomans@gmail.com>
2014-02-02 23:15:21 -0800

    Fixed typo

--- Segway/Segue/UIViewController+Popover.m
+++ Segway/Segue/UIViewController+Popover.m
@@ -9,16 +9,16 @@
 #import "UIViewController+Popover.h"
 #import <objc/runtime.h>
 
-static char const * const UUIViewControllerPopoverViewControllerKey = "UUIViewControllerPopoverViewControllerKey";
+static char const * const UIViewControllerPopoverViewControllerKey = "UIViewControllerPopoverViewControllerKey";
 
 @implementation UIViewController (Popover)
 
 - (UIPopoverController*)popoverViewController {
-    return objc_getAssociatedObject(self, UUIViewControllerPopoverViewControllerKey);
+    return objc_getAssociatedObject(self, UIViewControllerPopoverViewControllerKey);
 }
 
 - (void)setPopoverViewController:(UIPopoverController*)popoverViewController {
-    objc_setAssociatedObject(self, UUIViewControllerPopoverViewControllerKey, popoverViewController, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    objc_setAssociatedObject(self, UIViewControllerPopoverViewControllerKey, popoverViewController, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
 }
 
 @end
